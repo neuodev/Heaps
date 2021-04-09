71aa0ebce5bd21f4045e96402612c675bc0852c3
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-09 16:54:30 +0200

    Create the insert method

--- Heap.ts
+++ Heap.ts
@@ -1,3 +1,24 @@
 // left = parent * 2 + 1
 // right = parent * 2 + 2
 // parent = (idx -1 ) / 2
+
+class Heap {
+  array: number[];
+  size: number;
+  constructor() {
+    this.array = [];
+    this.size = 0;
+  }
+
+  insert(val) {
+    this.array[this.size++] = val;
+  }
+}
+
+const heap = new Heap();
+heap.insert(20);
+heap.insert(30);
+heap.insert(40);
+heap.insert(50);
+heap.insert(60);
+console.log(heap.array);
