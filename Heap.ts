a40c1c36a0a7f0bda736b51e2e2eef2870ccd5b6
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-11 10:01:52 +0200

    Add helper methods

--- Heap.ts
+++ Heap.ts
@@ -15,6 +15,26 @@ class Heap {
     this.bubbleUp();
   }
 
+  remove() {
+    this.swap(0, this.size - 1);
+    this.array.pop();
+    this.bubbleDown();
+    console.log(this.array);
+  }
+  bubbleDown() {
+    let idx = 0;
+    let array = this.array;
+    while (this.isValidParent(array, idx)) {
+      if (array[idx] < this.rightChild(idx)) {
+        this.swap(idx, this.rightChildIdx(idx));
+        idx = this.rightChildIdx(idx);
+      } else {
+        this.swap(idx, this.leftChildIdx(idx));
+        idx = this.leftChildIdx(idx);
+      }
+    }
+  }
+
   bubbleUp() {
     let idx = this.size - 1;
 
@@ -30,6 +50,25 @@ class Heap {
   swap(i, j) {
     [this.array[i], this.array[j]] = [this.array[j], this.array[i]];
   }
+
+  leftChildIdx(idx) {
+    return idx * 2 + 1;
+  }
+  rightChildIdx(idx) {
+    return idx * 2 + 2;
+  }
+  leftChild(idx) {
+    return this.array[this.leftChildIdx(idx)];
+  }
+  rightChild(idx) {
+    return this.array[this.rightChildIdx(idx)];
+  }
+
+  isValidParent(array, idx) {
+    return (
+      array[idx] >= this.leftChild(idx) && array[idx] >= this.rightChild(idx)
+    );
+  }
 }
 
 const heap = new Heap();
@@ -38,4 +77,5 @@ heap.insert(30);
 heap.insert(50);
 heap.insert(60);
 heap.insert(70);
+heap.remove();
 console.log(heap.array);
