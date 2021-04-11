b3068c7592799530dfb53e2efce2864c973db685
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-11 09:38:19 +0200

    Add the insert method

--- Heap.ts
+++ Heap.ts
@@ -12,13 +12,30 @@ class Heap {
 
   insert(val) {
     this.array[this.size++] = val;
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.size - 1;
+
+    while (idx > 0 && this.array[idx] > this.array[this.parent(idx)]) {
+      this.swap(idx, this.parent(idx));
+      idx = this.parent(idx);
+    }
+  }
+
+  parent(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+  swap(i, j) {
+    [this.array[i], this.array[j]] = [this.array[j], this.array[i]];
   }
 }
 
 const heap = new Heap();
 heap.insert(20);
 heap.insert(30);
-heap.insert(40);
 heap.insert(50);
 heap.insert(60);
+heap.insert(70);
 console.log(heap.array);
